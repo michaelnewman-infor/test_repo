2b3f2aab28394280c124691595c4afdb3d228e04
Michael Newman <michael.newman@infor.com>
2023-01-30 14:06:31 -0500

    add outputs.tf

---
+++ modules/outputs.tf
@@ -0,0 +1,19 @@
+output "first" {
+  value     = "first_value"
+  sensitive = true
+}
+
+output "second" {
+  value     = "second_value"
+  sensitive = false
+}
+
+output "third" {
+  value     = "third_value"
+  sensitive = true
+}
+
+output "fourth" {
+  value     = "fourth_value"
+  sensitive = false
+}
