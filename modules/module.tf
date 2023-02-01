2b359ae979a9bbd2f55bc8832496e9e0b1a6d27c
Michael Newman <michael.newman@infor.com>
2023-02-01 15:39:56 -0500

    update module.tf

--- modules/module.tf
+++ modules/module.tf
@@ -1,38 +1,38 @@
 # Simulate creating a resource which takes a minute.
-resource "time_sleep" "wait_60_seconds" {
-  create_duration = "60s"
+resource "time_sleep" "wait_120_seconds" {
+  create_duration = "120s"
 }
 
 resource "null_resource" "next" {
-  depends_on = [time_sleep.wait_60_seconds]
+  depends_on = [time_sleep.wait_120_seconds]
 }
 
-output "first" {
+output "a" {
   value     = "first_value"
   sensitive = true
 }
 
-output "second" {
+output "b" {
   value     = "second_value"
   sensitive = false
 }
 
-output "third" {
+output "c" {
   value     = "third_value"
   sensitive = true
 }
 
-output "fourth" {
+output "d" {
   value     = "fourth_value"
   sensitive = false
 }
 
-output "fifth" {
+output "e" {
   value     = "fifth_value"
   sensitive = false
 }
 
-output "sixth" {
+output "f" {
   value     = "sixth_value"
   sensitive = true
 }
